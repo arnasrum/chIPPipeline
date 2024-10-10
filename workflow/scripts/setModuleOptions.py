b7b5b047d0a85b7aad0871c116effbd238131764
Arnas Rumbavicius <a.rumbavicius@gmail.com>
2024-10-10 15:29:42 +0200

    Added parsing of modules config key word

---
+++ workflow/scripts/setModuleOptions.py
@@ -0,0 +1,37 @@
+import re
+
+def setModuleOptions(config: dict) -> None:
+    '''
+        Reads config["modules"] and overwrites the default values.
+        Optimally specified when running snakemake through CLI 
+        with --config modules="--flag value -f value"
+    '''
+    # --flag- is not caught as a flag
+    # multi character flag with single dash is read as value
+    pattern = re.compile(r"(?i)\B--[a-z]*\s|\B-[a-z]\s")
+    flags = pattern.findall(config["modules"])
+    for i in range(len(flags)):
+        argumentStart = re.search(flags[i], config["modules"]).end()
+        if i == len(flags) - 1:
+            argumentEnd = len(config["modules"])
+        else:
+            argumentEnd = re.search(flags[i + 1], config["modules"]).start()
+        argument = config["modules"][argumentStart: argumentEnd]
+        match flags[i].rstrip():
+            case "-t":
+                __setConfigOption(config, "trimmer", argument)
+            case "--trim":
+                __setConfigOption(config, "trimmer", argument)
+            case "-a":
+                __setConfigOption(config, "alginer", argument)
+            case "--align":
+                __setConfigOption(config, "alginer", argument)
+            case "-g":
+                __setConfigOption(config, "genome", argument)
+            case "--genome":
+                __setConfigOption(config, "genome", argument)
+            case _:
+                raise NotImplementedError(f"{flags[i]} flag is not supported")
+
+def __setConfigOption(config:dict, option: str, value: str) -> None:
+    config[option] = value.rstrip().lstrip()
